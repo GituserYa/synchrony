fdce2f1c11f7b238e41f224d88b36fc074db1fd2
GituserYa <149683612+GituserYa@users.noreply.github.com>
2024-12-26 12:03:48 +0530

    Commit for

--- src/main/java/com/example/synchrony/controller/RegisterController.java
+++ src/main/java/com/example/synchrony/controller/RegisterController.java
@@ -1,10 +1,21 @@
 package com.example.synchrony.controller;
 
+import com.example.synchrony.Exception.CustomException;
+import com.example.synchrony.Exception.DBException;
+import com.example.synchrony.dto.GetUserResponseDTO;
 import com.example.synchrony.dto.RegisterDTO;
+import com.example.synchrony.dto.ResponseDTO;
+import com.example.synchrony.entity.User;
 import com.example.synchrony.service.UserService;
+import jakarta.websocket.server.PathParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RequestMapping("/api/register")
 @RestController
@@ -14,25 +25,33 @@ public class RegisterController {
     /**
      * Dependency injection for the user service
      **/
-    private final UserService userService;
+    @Autowired
+    private UserService userService;
 
-    public RegisterController(UserService userService) {
-        this.userService = userService;
-    }
 
     /**
      * Post Mapping for saving the @username and @password
-     * @param RegisterDTO in the Imgur Software
-     *  First save the username and password
+     *
+     * @param RegisterDTO
      **/
     @PostMapping("/user")
-    public String saveDetails(@RequestBody RegisterDTO registerDTO) {
-        logger.info("Received request to register user: {}", registerDTO.getUsername());
-        try {
-            return userService.save(registerDTO);
-        } catch (Exception e) {
-            logger.error("An error occurred while processing the request: {}", e.getMessage());
-            return "An error occurred. Please try again later.";
-        }
+    public ResponseEntity<ResponseDTO> saveDetails(@RequestBody RegisterDTO registerDTO) throws CustomException {
+        logger.info("Received request to register user: {}", registerDTO);
+        userService.saveData(registerDTO);
+        return new ResponseEntity<>(new ResponseDTO("DATA SAVED SUCCESSFULLY"), HttpStatus.OK);
+    }
+
+    @GetMapping("/userDetails/{id}")
+    public ResponseEntity<GetUserResponseDTO> getUserDetails(@PathVariable Long id) throws DBException {
+        logger.info("Received request to find user with id : {}", id);
+       var response= userService.getRecordById(id);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/userDetails/{id}")
+    public ResponseEntity<ResponseDTO> deleteUserDetails(@PathVariable Long id) throws DBException {
+        logger.info("Received request to find user with id : {}", id);
+      userService.deleteRecord(id);
+        return new ResponseEntity<>(new ResponseDTO("DATA DELETED SUCCESFFULLY"), HttpStatus.OK);
     }
 }
